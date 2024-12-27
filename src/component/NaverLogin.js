53f374fbdb714da10b7abaf474f4be8ae22542fc
minhyo00 <dlalsgy1234@gmail.com>
2024-12-27 20:00:48 +0900

    feat: dev naver login

--- src/component/NaverLogin.js
+++ src/component/NaverLogin.js
@@ -1,33 +1,26 @@
-import { useEffect } from 'react'
+import React from 'react';
 
 const NaverLogin = () => {
+    const NAVER_CLIENT_ID = process.env.REACT_APP_Naver_CLIENT_ID;
+    const NAVER_REDIRECT_URI = "http://localhost:3000/test";  // 리다이렉트 URL
+    const NAVER_AUTH_URL = "https://nid.naver.com/oauth2.0/authorize";
 
-    const { naver } = window
+    const handleLogin = () => {
+        const redirectUri = encodeURIComponent(NAVER_REDIRECT_URI);
+        const naverLoginUrl = `${NAVER_AUTH_URL}?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${redirectUri}&state=RANDOM_STATE`;
 
-    const NAVER_CLIENT_ID = process.env.REACT_APP_Naver_CLIENT_ID
-    const NAVER_CALLBACK_URL = process.env.REACT_APP_Naver_CALLBACK_URL
+        // 네이버 로그인 페이지로 리다이렉트
+        window.location.href = naverLoginUrl;
+    };
 
-    const initializeNaverLogin = () => {
-        const naverLogin = new naver.LoginWithNaverId({
-            clientId: NAVER_CLIENT_ID,
-            callbackUrl: NAVER_CALLBACK_URL,
-            // 팝업창으로 로그인을 진행할 것인지?           
-            isPopup: false,
-            // 버튼 타입 ( 색상, 타입, 크기 변경 가능 )
-            loginButton: { color: 'green', type: 3, height: 58 },
-            callbackHandle: true,
-        })
-        naverLogin.init()
-    }
-
-    useEffect(() => {
-        initializeNaverLogin()
-    }, [])
-    return(
-        <>
-            <div id="naverIdLogin" />
-        </>
-    )
-}
+    return (
+        <img
+            src="btnG.png"
+            alt="네이버 로그인"
+            onClick={handleLogin}
+            style={{ cursor: 'pointer', width: '270px', height: '58px' }}
+        />
+    );
+};
 
 export default NaverLogin;
--- src/pages/Test1.js
+++ src/pages/Test1.js
@@ -1,11 +1,33 @@
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
+const Test1 = () => {
+    const [searchParams] = useSearchParams(); // URL의 쿼리 파라미터를 가져옴
 
-const Test1 = () =>{
-    return(
+    useEffect(() => {
+        getData()
+        const code = searchParams.get('code');
+        const state = searchParams.get('state');
+
+        if (code) {
+            console.log('Authorization Code:', code);
+            console.log('State:', state);
+        } else {
+            console.log('No Authorization Code found.');
+        }
+    }, [searchParams]);
+
+    return (
         <div>
-            <h1>Test</h1>
+            <h1>네이버 로그인 리다이렉트 페이지</h1>
+            <p>
+                <strong>Authorization Code:</strong> {searchParams.get('code') || 'Not Found'}
+            </p>
+            <p>
+                <strong>State:</strong> {searchParams.get('state') || 'Not Found'}
+            </p>
         </div>
-    )
-}
+    );
+};
 
 export default Test1;
